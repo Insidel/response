f6ff80b94f34f626914804949d7d7ca38cef4627
Martin <Insidel@seznam.cz>
2014-03-13 20:19:19 +0100

    - treti final - projekt

---
+++ Response-mois/Response-mois/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Response_mois.Startup))]
+namespace Response_mois
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
