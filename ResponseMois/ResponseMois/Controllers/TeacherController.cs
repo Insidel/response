291b8cabe5c7bdae9a1961c7e4d862506089194f
Martin <Insidel@seznam.cz>
2014-04-12 00:35:10 +0200

    / pridana editace kurzu

--- ResponseMois/ResponseMois/Controllers/TeacherController.cs
+++ ResponseMois/ResponseMois/Controllers/TeacherController.cs
@@ -58,7 +58,7 @@ namespace ResponseMois.Controllers
             ViewBag.Message = "Úprava studentů.";
 
             UserService userService = new UserService();
-            IList<User> userList = userService.GetAll("from User");
+            IList<User> userList = userService.GetAll("from User"); //TODO WHERE role = student
 
             ViewBag.students = userList;
             return View();
@@ -73,7 +73,40 @@ namespace ResponseMois.Controllers
 
             ViewBag.Message = "Úprava kurzů.";
 
+            CourseService courseService = new CourseService();
+            IList<Course> courseList = courseService.GetAll("from Course"); //TODO teacher_id is user
+
+            ViewBag.courses = courseList;
+
             return View();
+        }
+        //
+        // GET: /Teacher/CourseAdd
+        public ActionResult CourseAdd()
+        {
+            return View();
+        }
+
+
+        //
+        // POST: /Teacher/CourseAdd
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CourseAdd(Course model)
+        {
+            if (ModelState.IsValid)
+            {
+                Course c = new Course();
+                c.name = model.name;
+                c.schoolYear = model.schoolYear;
+                c.meetingTime = model.meetingTime;
+                c.note = model.note;
+               //TODO c.teacher_id = this.user.id;
+                CourseService courseService = new CourseService();
+                courseService.Persist(c);
+            }
+
+            return View(model);
         }
 
         //
